99b244d1399e062dea848df278a1ae53e2782979
Marco Roganovic <marcoroganovic@gmail.com>
2017-12-31 11:14:12 +0100

    add /graphql endpoint and object types

---
+++ schema.js
@@ -0,0 +1,20 @@
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require("graphql");
+
+module.exports = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
+});
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const graphqlHTTP = require('express-graphql');
+const schema = require("./schema");
 
-app.use("/graphql", (res, req, next) => {
-  res.send("GraphiQL will be here!");
-});
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 app.listen(3000, (err) => {
   if(err) throw err;
